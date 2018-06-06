697dec952db7c8b119d30c85444f0e60d0b571e6
ke2li <li.kyle25@gmail.com>
2018-06-06 19:05:07 -0400

    Completed day4

---
+++ day4/consoleLog.js
@@ -0,0 +1,13 @@
+console.log('1 : hello world !');
+
+console.log("2: this will also work");
+
+var str = 'nodejsera';
+
+var val = 25;
+
+console.log(str);
+
+console.log('4: value of val is : %d', val);
+console.log('5: %s' , 'this will be printed after');
+console.log("6 : str = " + str);
---
+++ day4/consoleWarn.js
@@ -0,0 +1,9 @@
+var x= 10;
+var y= 20;
+var result = x/y;
+
+if(result % 2 == 0) {
+	console.log("result : %d".result);
+}else{
+	console.warn("warning : decimal number");
+}
