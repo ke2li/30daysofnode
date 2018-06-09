13d5f86151e911d4bed476d3b18eb817e92a9bac
ke2li <li.kyle25@gmail.com>
2018-06-08 20:38:17 -0400

    Completed day6

---
+++ day6/indexOf.js
@@ -0,0 +1,8 @@
+arr = [5,2,8,5,6];
+console.log(arr);
+var pos = arr.indexOf(8);
+console.log("index of 8 is : " + pos);
+var pos1 = arr.indexOf(5);
+console.log("index of 5 is : " + pos1);  
+var pos2 = arr.lastIndexOf(5);
+console.log("last index of 5 is : " + pos2);
