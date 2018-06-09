15d71eefda5699f5275dfdd87f603cc2a19ee0dd
ke2li <li.kyle25@gmail.com>
2018-06-09 11:19:19 -0400

    Completed day8

---
+++ day8/export.js
@@ -0,0 +1,7 @@
+var funcName1 = function(data){
+	console.log(data);
+}
+
+module.exports = {
+	function_1 : funcName1
+};
---
+++ day8/test.js
@@ -0,0 +1,4 @@
+var fun = require('./export.js');
+var param = ["a", "demo", "array"];
+var output = fun.function_1(param);
+console.log(output);
